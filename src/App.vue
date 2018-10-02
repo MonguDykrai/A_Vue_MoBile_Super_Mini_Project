2123a92aa7c7584bdaa87c5eae3721ff9d318199
Xu <zhen.xu@hp.com>
2018-10-02 21:59:30 +0800

    修改了公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,121 @@
 <template>
   <div id="app">
-    <mt-header title="無題" :style="{fontFamily: 'Microsoft YaHei Light'}" :class="{'is-fixed': fixed}" :fixed="fixed">
-      <router-link to="/" slot="left">
-        <mt-button icon="back"></mt-button>
-      </router-link>
-    </mt-header>
+    
+    <div class="marginTop"> <!-- 公共的头部间距和底部间距 -->
+
+      <mt-header title="無題" :style="{fontFamily: 'Microsoft YaHei Light'}" :class="{'is-fixed': fixed}" :fixed="fixed">
+        <router-link to="/" slot="left">
+          <mt-button icon="back"></mt-button>
+        </router-link>
+      </mt-header>
 
     <router-view />
 
-    <mt-tabbar v-model="selected" :class="{'is-fixed': fixed}" :fixed="fixed">
-      <!-- v-model="selected" cannot be deleted -->
-      <mt-tab-item id="iHome">
-        <i slot="icon" class="iconfont icon-ioshomeoutline"></i>
-        Home
-      </mt-tab-item>
-      <mt-tab-item id="iMember">
-        <i slot="icon" class="iconfont icon-profileo"></i>
-        Member
-      </mt-tab-item>
-      <mt-tab-item id="iCart">
-        <i slot="icon" class="iconfont icon-cart"></i>
-        Cart
-      </mt-tab-item>
-      <mt-tab-item id="iSearch">
-        <i slot="icon" class="iconfont icon-search"></i>
-        Search
-      </mt-tab-item>
-    </mt-tabbar>
+      <mt-tabbar v-model="selected" :class="{'is-fixed': fixed}" :fixed="fixed">
+        <!-- v-model="selected" cannot be deleted -->
+        <mt-tab-item id="iHome">
+          <i slot="icon" class="iconfont icon-ioshomeoutline"></i>
+          Home
+        </mt-tab-item>
+        <mt-tab-item id="iMember">
+          <i slot="icon" class="iconfont icon-profileo"></i>
+          Member
+        </mt-tab-item>
+        <mt-tab-item id="iCart">
+          <i slot="icon" class="iconfont icon-cart"></i>
+          Cart
+        </mt-tab-item>
+        <mt-tab-item id="iSearch">
+          <i slot="icon" class="iconfont icon-search"></i>
+          Search
+        </mt-tab-item>
+      </mt-tabbar>
+
+    </div>
+
+    
 
   </div>
 </template>
 
 <script>
-
-  export default {
-    name: 'App',
-    data: function () {
-      return {
-        selected: null, // value of selected tab-item's id, name can be anything (e.g. value)
-        fixed: true
-      }
-    },
-    watch: {
-      // Which link has been clicked
-      selected: function (newVal) {
-        let compID = newVal
-        switch (compID) {
-          case 'iHome':
-            /*
+export default {
+  name: "App",
+  data: function() {
+    return {
+      selected: null, // value of selected tab-item's id, name can be anything (e.g. value)
+      fixed: true
+    };
+  },
+  watch: {
+    // Which link has been clicked
+    selected: function(newVal) {
+      let compID = newVal;
+      switch (compID) {
+        case "iHome":
+          /*
              * this.$route
              * {name: "HelloWorld", meta: {…}, path: "/", hash: "", query: {…}, …}
              * 
              * this.$router
              * access to the router instance
              */
-            this.$router.push('/')
-            break;
-          case 'iMember':
-            this.$router.push('/member')
-            break;
-          case 'iCart':
-            this.$router.push('/cart')
-            break;
-          case 'iSearch':
-            this.$router.push('/search')
-            break;
-        }
+          this.$router.push("/");
+          break;
+        case "iMember":
+          this.$router.push("/member");
+          break;
+        case "iCart":
+          this.$router.push("/cart");
+          break;
+        case "iSearch":
+          this.$router.push("/search");
+          break;
       }
     }
   }
+};
 </script>
 
 <style scoped>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+.marginTop {
+  padding-top: 40px;
+  padding-bottom: 51px;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+} */
 
-  .mint-tabbar {
-    /* 默认高度是由 icon 和 font 的高度撑起来的，先写死 */
-    /* height: 50px; */
+.mint-tabbar {
+  /* 默认高度是由 icon 和 font 的高度撑起来的，先写死 */
+  /* height: 50px; */
 
-    background-color: #F7F7F7;
-    border-top: 1px solid #959598;
-  }
+  background-color: #f7f7f7;
+  border-top: 1px solid #959598;
+}
 
-  .mint-tab-item {
-    padding: 5px 0;
-  }
+.mint-tab-item {
+  padding: 5px 0;
+}
 
+.mint-tabbar > .mint-tab-item.is-selected {
+  background-color: inherit;
+}
 
-  .mint-tabbar>.mint-tab-item.is-selected {
-    background-color: inherit;
-  }
+.mint-tab-item-icon > * {
+  font-size: 24px;
+}
 
-  .mint-tab-item-icon>* {
-    font-size: 24px;
-  }
-
-  .mint-tab-item-label {
-    font-size: 11px;
-    /* 为啥不管用 */
-  }
+.mint-tab-item-label {
+  font-size: 11px;
+  /* 为啥不管用 */
+}
 </style>
--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="c-cart">
+  
     <h1>{{ msg }}</h1>
-  </div>
+ 
 </template>
 
 <script>
@@ -17,8 +17,5 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .c-cart {
-    padding-top: 40px;
-    padding-bottom: 51px;
-  }
+ 
 </style>
